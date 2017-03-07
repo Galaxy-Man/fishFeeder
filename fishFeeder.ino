b4db2e7cbffe39740505434812b0f40924358fe2
mazhigbee <mhigb411@mail.endicott.edu>
2017-03-07 11:14:18 -0500

    TESTING is enabled in this version
    LINES 18 and 19 need to be commented out unless you are testing

--- fishFeeder.ino
+++ fishFeeder.ino
@@ -12,11 +12,11 @@ int pos = 0;    // variable to store the servo position
 
 void setup() {
 	Serial.begin(9600); //init serial connection at 9600 baud
-	setTime(7, 59, 45, 3, 5, 17); //set the time to 12:55:00 3/5/2017
+	setTime(7, 59, 45, 3, 7, 17); //set the time to 12:55:00 3/5/2017
 	
 	Alarm.alarmRepeat(8,0,0,feedAlarm); //alarm to feed fish goes off at 8am
-	// Alarm.alarmRepeat(8,0,30,feedAlarm);  //uncomment these to test on 30 second intervals 3 times
-	// Alarm.alarmRepeat(8,1,0,feedAlarm);
+	Alarm.alarmRepeat(8,0,30,feedAlarm);  //uncomment these to test on 30 second intervals 3 times
+	Alarm.alarmRepeat(8,1,0,feedAlarm);
 	Alarm.alarmRepeat(12,0,0,feedAlarm);//alarm set for 12pm
 	Alarm.alarmRepeat(16,0,0,feedAlarm);//alarm set for 4pm
 	myservo.attach(10);  // attaches the servo on pin 10 to the servo object
